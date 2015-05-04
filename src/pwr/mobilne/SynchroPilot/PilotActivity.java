5f991ecea89d2f06d0c0da877ee0ab04b969a475
Dasiuss <Dasiuss@gmail.com>
2015-05-05 00:23:14 +0200

    Ports scanning lets us find our PC Server (needs to listen on 9562)

--- src/pwr/mobilne/SynchroPilot/PilotActivity.java
+++ src/pwr/mobilne/SynchroPilot/PilotActivity.java
@@ -1,15 +1,15 @@
 package pwr.mobilne.SynchroPilot;
 
+import pwr.mobilne.SynchroPilot.controller.ConnectionController;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PilotActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class PilotActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		ConnectionController.getInstance().prepareSocket(getApplicationContext());
+	}
 }
--- src/pwr/mobilne/SynchroPilot/controller/ConnectionController.java
+++ src/pwr/mobilne/SynchroPilot/controller/ConnectionController.java
@@ -5,10 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.util.Log;
 
+/**
+ * in Activity, call <code>ConnectionController.getInstance().prepareSocket(getApplicationContext());</code>
+ * 
+ */
 public class ConnectionController {
 	private static ConnectionController instance = null;
 
@@ -18,12 +30,19 @@ public class ConnectionController {
 	private BufferedReader in;
 	private Boolean socketReady = false;
 
+	private Context context;
+
 	public synchronized void sendToSocket(String msg) {
 		if (socketReady) out.println(msg);
 	}
 
-	public void prepareSocket() {
-		new ConnectionTask().execute("");
+	public void prepareSocket(Context context) {
+		this.context = context;
+		String ip = getWifiIpAddress(context);
+		ip = ip.substring(0, ip.lastIndexOf("."));
+		for (int i = 1; i < 255; i++) {
+			new ConnectionTask().execute(ip + "." + i);
+		}// TODO save last positive try, to avoid rescanning after onResume();
 	}
 
 	protected void setSocket(Socket socket) {
@@ -38,26 +57,34 @@ public class ConnectionController {
 		socketReady = true;
 	}
 
+	/**
+	 * call <code>new ConnectionTask().execute(ipAddress);</code>
+	 */
 	private class ConnectionTask extends AsyncTask<String, Integer, Socket> {
 
 		@Override
 		protected Socket doInBackground(String... params) {
-			String host = "192.168.1.155";
-			int port = 3456;
+
+			String host = params[0];
+			int port = 9562;
 			Socket socket = null;
-			try {
-				socket = new Socket(host, port);
-			} catch (IOException e) {
-				this.cancel(false);
-				System.out.println("Unknown host");
-				// System.exit(1);
-			}
+			if ((socket = portIsOpen(host, port, 1000)) == null) this.cancel(false);
 			return socket;
 		}
 
 		@Override
 		protected void onPostExecute(Socket result) {
 			ConnectionController.getInstance().setSocket(result);
+		}
+
+		public Socket portIsOpen(String ip, int port, int timeout) {
+			try {
+				Socket socket = new Socket();
+				socket.connect(new InetSocketAddress(ip, port), timeout);
+				return socket;
+			} catch (Exception ex) {
+				return null;
+			}
 		}
 	}
 
@@ -67,6 +94,28 @@ public class ConnectionController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected String getWifiIpAddress(Context context) {
+		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+
+		// Convert little-endian to big-endianif needed
+		if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+			ipAddress = Integer.reverseBytes(ipAddress);
+		}
+
+		byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+
+		String ipAddressString;
+		try {
+			ipAddressString = InetAddress.getByAddress(ipByteArray).getHostAddress();
+		} catch (UnknownHostException ex) {
+			Log.e("WIFIIP", "Unable to get host address.");
+			ipAddressString = null;
+		}
+
+		return ipAddressString;
 	}
 
 	public static ConnectionController getInstance() {
